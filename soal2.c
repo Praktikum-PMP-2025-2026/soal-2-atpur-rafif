8318335b3e2aefa5ce9d77b106e4449aaaafa83b
atpur-rafif <muhammadatpurrafif@gmail.com>
2026-04-15 08:35:48 +0700

    feat: implement soal

--- soal2.c
+++ soal2.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
-int main(void) {
-    return 0;
+int recurse(int n){
+	if(n == 1) return 0;
+	return 1 + (n % 2 == 0 ? recurse(n / 2) : recurse(3 * n + 1));
+}
+
+int main(){
+	int t;
+	scanf("%d", &t);
+
+	for(int i = 0; i < t; ++i){
+		int n;
+		scanf("%d", &n);
+
+		printf("LANGKAH %d\n", recurse(n));
+	}
 }
